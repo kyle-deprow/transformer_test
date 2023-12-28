d218d3fe0a2c0179dbe33fdf0adcf97cef839ff8
Kyle DeProw <kydepro@gmail.com>
2023-12-28 00:57:01 -0600

    added params to run.sh and generalized run.py

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Initializing training of NLP model..."
-python3 run.py --fine_tune /Users/ubuntu/Documents/ML/NN/rnn/config.json /Users/ubuntu/Documents/ML/NN/rnn/data/bee_gees.txt /Users/ubuntu/Documents/ML/NN/rnn/models/model_bee_gees.json
+python3 run.py --fine_tune config.json shakespeare.txt model_shakespeare.json -load_path model_params_large.json
