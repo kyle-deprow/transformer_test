79151747e8ac6340d75adc65fb9c415e8d7dd8db
Kyle DeProw <kydepro@gmail.com>
2023-12-30 00:00:08 -0600

    replaced np.exp with torch.exp for LSTM calcs

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Initializing training of NLP model..."
+<<<<<<< HEAD
 python3 run.py --train config.json shakespeare.txt -to_path shakespeare.json -from_path model_params_large.json
+=======
+python3 run.py --train config.json shakespeare.txt -to_path shakespeare.json -from_path sanderson.json
+>>>>>>> 5bfbb29... fixed LSTM for torch
